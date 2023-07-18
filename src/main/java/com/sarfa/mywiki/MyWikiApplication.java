c83b92b572af677ff5aba496bcd3abaa6197f873
SARFA <1498627775@qq.com>
2023-07-18 14:38:15 +0800

    1.1 修改启动图案

--- src/main/java/com/sarfa/mywiki/MyWikiApplication.java
+++ src/main/java/com/sarfa/mywiki/MyWikiApplication.java
@@ -9,10 +9,13 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 public class MyWikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(MyWikiApplication.class);
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MyWikiApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
         LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
+    //启动图案设置
+    //http://patorjk.com/software/taag/#p=display&h=1&v=1&f=Big&t=jiawa%0A
 }
