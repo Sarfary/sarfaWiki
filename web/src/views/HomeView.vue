83fe6b6a11400defcfefdee9bcb3dfe135e5597e
SARFA <1498627775@qq.com>
2023-07-22 15:27:40 +0800

    1.6 使用ant-design的一个列表组件，显示电子书

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -12,7 +12,7 @@
             <a-sub-menu key="sub1">
               <template #title>
                 <span>
-                  <user-outlined />
+                  <user-outlined/>
                   subnav 1
                 </span>
               </template>
@@ -24,7 +24,7 @@
             <a-sub-menu key="sub2">
               <template #title>
                 <span>
-                  <laptop-outlined />
+                  <laptop-outlined/>
                   subnav 2
                 </span>
               </template>
@@ -36,7 +36,7 @@
             <a-sub-menu key="sub3">
               <template #title>
                 <span>
-                  <notification-outlined />
+                  <notification-outlined/>
                   subnav 3
                 </span>
               </template>
@@ -48,15 +48,77 @@
           </a-menu>
         </a-layout-sider>
         <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-          <pre>{{ebooks}}</pre>
+          <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+            <template #footer>
+              <div>
+                <b>ant design vue</b>
+                footer part
+              </div>
+            </template>
+            <template #renderItem="{ item }">
+              <a-list-item key="item.title">
+                <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+                </template>
+                <template #extra>
+                  <img
+                      width="272"
+                      alt="logo"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                  />
+                </template>
+                <a-list-item-meta :description="item.description">
+                  <template #title>
+                    <a :href="item.href">{{ item.title }}</a>
+                  </template>
+                  <template #avatar>
+                    <a-avatar :src="item.avatar"/>
+                  </template>
+                </a-list-item-meta>
+                {{ item.content }}
+              </a-list-item>
+            </template>
+          </a-list>
         </a-layout-content>
       </a-layout>
     </a-layout-content>
   </a-layout>
 </template>
+<script lang="ts" setup>
+import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
+const pagination = {
+  onChange: (page: number) => {
+    console.log(page);
+  },
+  pageSize: 3,
+};
+const actions: Record<string, any>[] = [
+  {icon: StarOutlined, text: '156'},
+  {icon: LikeOutlined, text: '156'},
+  {icon: MessageOutlined, text: '2'},
+];
+</script>
 
 <script lang="ts">
-import { defineComponent ,onMounted, ref} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import axios from "axios";
 
 export default defineComponent({
@@ -76,7 +138,7 @@ export default defineComponent({
       );
     })
 
-    return{
+    return {
       ebooks
     }
 
