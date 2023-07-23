9d01cb5fced0ea2ee598118b2580cae424cf4686
SARFA <1498627775@qq.com>
2023-07-23 20:55:43 +0800

    1.8 后端实现分页查询ebook/list，并封装分页请求参数和返回参数

--- src/main/java/com/sarfa/mywiki/controller/EbookController.java
+++ src/main/java/com/sarfa/mywiki/controller/EbookController.java
@@ -3,13 +3,13 @@ package com.sarfa.mywiki.controller;
 import com.sarfa.mywiki.req.EbookReq;
 import com.sarfa.mywiki.resp.CommonResp;
 import com.sarfa.mywiki.resp.EbookResp;
+import com.sarfa.mywiki.resp.PageResp;
 import com.sarfa.mywiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ebook")
@@ -18,9 +18,9 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
     @GetMapping("/list")
-    public CommonResp<List<EbookResp>> list(EbookReq req){
-        CommonResp<List<EbookResp>> objectCommonResp = new CommonResp<>();
-        List<EbookResp> list= ebookService.list(req);
+    public CommonResp<PageResp<EbookResp>> list(EbookReq req){
+        CommonResp<PageResp<EbookResp>> objectCommonResp = new CommonResp<>();
+        PageResp<EbookResp> list= ebookService.list(req);
         objectCommonResp.setContent(list);
         return objectCommonResp;
     }
--- src/main/java/com/sarfa/mywiki/service/EbookService.java
+++ src/main/java/com/sarfa/mywiki/service/EbookService.java
@@ -1,10 +1,13 @@
 package com.sarfa.mywiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sarfa.mywiki.domain.Ebook;
 import com.sarfa.mywiki.domain.EbookExample;
 import com.sarfa.mywiki.mapper.EbookMapper;
 import com.sarfa.mywiki.req.EbookReq;
 import com.sarfa.mywiki.resp.EbookResp;
+import com.sarfa.mywiki.resp.PageResp;
 import com.sarfa.mywiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -18,13 +21,21 @@ public class EbookService {
 
     @Resource//这个是jdk自带的
     private EbookMapper ebookMapper;
-    public List<EbookResp> list(EbookReq req){
+    public PageResp<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
+        if(!ObjectUtils.isEmpty((req.getPage()))|!ObjectUtils.isEmpty(req.getSize())){
+            PageHelper.startPage(req.getPage(), req.getSize());
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        PageResp<EbookResp> objectPageResp = new PageResp<EbookResp>();
+        objectPageResp.setTotal(pageInfo.getTotal());
+        objectPageResp.setList(list);
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook:ebookList) {
 ////            EbookResp ebookResp = new EbookResp();
@@ -34,7 +45,7 @@ public class EbookService {
 //            respList.add(ebookResp);
 //        }
         //列表复制
-        return CopyUtil.copyList(ebookList, EbookResp.class);
+        return objectPageResp;
 
     }
 
