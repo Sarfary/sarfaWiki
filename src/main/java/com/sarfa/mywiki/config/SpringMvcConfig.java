3f6d38884235337f0b1e263744f29325756d43d3
SARFA <1498627775@qq.com>
2023-07-23 14:45:44 +0800

    1.7 添加AOP,显示请求参数，返回参数以及接口耗时，去掉过滤器和拦截器

--- src/main/java/com/sarfa/mywiki/config/SpringMvcConfig.java
+++ src/main/java/com/sarfa/mywiki/config/SpringMvcConfig.java
@@ -1,25 +1,17 @@
 package com.sarfa.mywiki.config;
 
-import com.sarfa.mywiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/login"
-                );
-
-    }
-}
-
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/login"
+//                );
+//
+//    }
+//}
