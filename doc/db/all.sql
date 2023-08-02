38ebcb5da0316c29843de61ead023a26e218b1d6
SARFA <1498627775@qq.com>
2023-08-02 16:50:38 +0800

    2.5 构建用户表，生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -96,15 +96,15 @@ insert into `category` (id, parent, name, sort) values (503, 500, '热门服务�
  ) engine=innodb default charset=utf8mb4 comment='文档内容';
 --
 -- -- 用户表
--- drop table if exists `user`;
--- create table `user` (
---   `id` bigint not null comment 'ID',
---   `login_name` varchar(50) not null comment '登陆名',
---   `name` varchar(50) comment '昵称',
---   `password` char(32) not null comment '密码',
---   primary key (`id`),
---   unique key `login_name_unique` (`login_name`)
--- ) engine=innodb default charset=utf8mb4 comment='用户';
+drop table if exists `user`;
+create table `user` (
+  `id` bigint not null comment 'ID',
+  `login_name` varchar(50) not null comment '登陆名',
+  `name` varchar(50) comment '昵称',
+  `password` char(32) not null comment '密码',
+  primary key (`id`),
+  unique key `login_name_unique` (`login_name`)
+) engine=innodb default charset=utf8mb4 comment='用户';
 --
 -- insert into `user` (id, `login_name`, `name`, `password`) values (1, 'test', '测试', 'e70e2222a9d67c4f2eae107533359aa4');
 --
