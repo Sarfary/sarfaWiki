1cd0b0e9f311503780538cd03ef9a360aa96a994
SARFA <1498627775@qq.com>
2023-08-03 15:26:12 +0800

    2.5 基本完成用户表的增删改查

---
+++ src/main/java/com/sarfa/mywiki/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.sarfa.mywiki.controller;
+
+import com.sarfa.mywiki.req.UserQueryReq;
+import com.sarfa.mywiki.req.UserSaveReq;
+import com.sarfa.mywiki.resp.UserQueryResp;
+import com.sarfa.mywiki.resp.CommonResp;
+import com.sarfa.mywiki.resp.PageResp;
+import com.sarfa.mywiki.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    /**
+     *
+     * @param req 分页 ，name模糊查询，分类查询, id查询
+     * @return CommonResp{
+     *   "success":
+     *   "message":
+     *   "content": {}
+     *   }
+     *
+     */
+    @GetMapping("/list")
+    public CommonResp<PageResp<UserQueryResp>> list(@Valid UserQueryReq req){
+        CommonResp<PageResp<UserQueryResp>> objectCommonResp = new CommonResp<>();
+        PageResp<UserQueryResp> list= userService.list(req);
+        objectCommonResp.setContent(list);
+        return objectCommonResp;
+    }
+    /**
+     *
+     * @param req 新增，更新的数据
+     * @return CommonResp
+     */
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        CommonResp objectCommonResp = new CommonResp<>();
+        userService.save(req);
+        return objectCommonResp;
+
+    }
+    /**
+     *
+     * @param id 根据id删除数据
+     * @return CommonResp
+     */
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id){
+        CommonResp objectCommonResp = new CommonResp<>();
+        userService.delete(id);
+        return objectCommonResp;
+    }
+
+}
---
+++ src/main/java/com/sarfa/mywiki/service/UserService.java
@@ -0,0 +1,76 @@
+package com.sarfa.mywiki.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.sarfa.mywiki.aspect.LogAspect;
+import com.sarfa.mywiki.domain.User;
+import com.sarfa.mywiki.domain.UserExample;
+import com.sarfa.mywiki.mapper.UserMapper;
+import com.sarfa.mywiki.req.UserQueryReq;
+import com.sarfa.mywiki.req.UserSaveReq;
+import com.sarfa.mywiki.resp.UserQueryResp;
+import com.sarfa.mywiki.resp.PageResp;
+import com.sarfa.mywiki.util.CopyUtil;
+import com.sarfa.mywiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserService {
+    //    @Autowired 也可以用这个来注入，这个是springboot自带的
+    //打印日志
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+    @Resource//这个是jdk自带的
+    private UserMapper userMapper;
+
+
+    public PageResp<UserQueryResp> list(UserQueryReq req) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        if (!ObjectUtils.isEmpty(req.getLoginName())) {
+            criteria.andLoginNameEqualTo(req.getLoginName());
+        }
+
+        //分页
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<User> userList = userMapper.selectByExample(userExample);
+
+        List<UserQueryResp> list = CopyUtil.copyList(userList, UserQueryResp.class);
+        PageInfo<User> pageInfo = new PageInfo<>(userList);
+        PageResp<UserQueryResp> objectPageResp = new PageResp<UserQueryResp>();
+        objectPageResp.setTotal(pageInfo.getTotal());
+        objectPageResp.setList(list);
+        return objectPageResp;
+
+    }
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public void save(UserSaveReq req) {
+        User user = CopyUtil.copy(req, User.class);
+        if (ObjectUtils.isEmpty(user.getId())) {
+            //id为空，是新增
+            user.setId(snowFlake.nextId());
+            userMapper.insert(user);
+
+        } else {
+            //id不为空，是更新
+            int a = userMapper.updateByPrimaryKey(user);
+            //显示是否执行
+            // LOG.info("{}",a);
+
+        }
+
+    }
+    public void delete(Long id){
+        userMapper.deleteByPrimaryKey(id);
+    }
+
+
+}
