05d5ffc5454a1a20e13b97a1e62ab5e597438c90
SARFA <1498627775@qq.com>
2023-08-08 14:53:26 +0800

    2.6 前端实现登录功能

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -2,6 +2,10 @@
   <a-layout id="the-header-style">
     <a-layout-header class="header">
       <div class="logo"/>
+      <a :style="{float:'right', color:'#bbb'}" @click="login">
+        <div class="loginLogo" v-if="!isLogin">登录</div>
+        <div class="loginLogo" v-else>我的</div>
+      </a>
       <a-menu
           theme="dark"
           mode="horizontal"
@@ -22,16 +26,110 @@
         <a-menu-item key="/about">
           <router-link to="/about">关于我们</router-link>
         </a-menu-item>
+
       </a-menu>
+
+      <a-modal
+          v-model:open="loginOpen"
+          title="登录"
+          :confirm-loading="loginConfirmLoading"
+          okText="Login"
+          @ok="handleLoginOk">
+        <a-form
+            :model="user"
+            name="basic"
+            :label-col="{ span: 4 }"
+            :wrapper-col="{ span: 18 }"
+            autocomplete="on"
+        >
+          <a-form-item label="用户名" :style="{marginTop:'20px'}">
+            <a-input v-model:value="user.loginName">
+              <template #prefix>
+                <user-outlined/>
+              </template>
+            </a-input>
+          </a-form-item>
+
+          <a-form-item label="密码" :style="{marginTop:'20px'}">
+            <a-input-password v-model:value="user.password">
+              <template #prefix>
+                <lock-outlined/>
+              </template>
+            </a-input-password>
+
+          </a-form-item>
+
+        </a-form>
+      </a-modal>
     </a-layout-header>
   </a-layout>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
+import {UserOutlined, LockOutlined} from "@ant-design/icons-vue"
+import {message} from "ant-design-vue";
+import {Tool} from "@/util/tool";
+import axios from "axios";
+declare let hexMd5: any;
+declare let KEY: any;
 
 export default defineComponent({
   name: 'the-header',
+  components:{
+    UserOutlined,
+    LockOutlined,
+  },
+  setup() {
+    const loginOpen = ref(false);
+    const loginConfirmLoading = ref(false);
+    const user = ref();
+    const isLogin = ref(false);
+    const handleLoginOk = () => {
+      console.log("login");
+      loginConfirmLoading.value = true;
+      const loginUser:any = {};
+      loginUser.value = Tool.copy(user.value);
+      loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+      axios.post("/user/login", {
+        loginName: loginUser.value.loginName,
+        password: loginUser.value.password
+      }).then((response)=>{
+        const data = response.data;
+        console.log(data);
+        loginConfirmLoading.value = false;
+        if(data.success){
+          loginOpen.value = false;
+          message.success("登录成功");
+        }
+        else {
+          message.error(data.message);
+        }
+      })
+
+    }
+    const login = () => {
+      user.value = {
+        loginName:'sarfa',
+        password:'root1234'
+      };
+      loginOpen.value = true;
+    }
+
+    return {
+      loginOpen,
+      loginConfirmLoading,
+      user,
+      isLogin,
+      handleLoginOk,
+      login
+
+
+      , UserOutlined
+      , LockOutlined
+
+    }
+  }
 });
 </script>
 
@@ -48,4 +146,19 @@ export default defineComponent({
   float: right;
   margin: 16px 0 16px 24px;
 }
+.loginLogo{
+  width: 44px;
+  height: 44px;
+  line-height: 44px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-radius: 50%;
+  text-align: center;
+  background: rgba(255, 255, 255, 0.1);
+}
+/*
+.loginLogo:hover{
+  transform: translateY(-2px);
+  box-shadow: 0 15px 30px rgba(255, 255, 255, 2);
+}*/
 </style>
